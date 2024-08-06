df781c97a3297b5479d6c9dce34ab49fbcb97059
YogSottot <7411302+YogSottot@users.noreply.github.com>
2024-08-06 22:15:55 +0600

    Add template for zabbix 7.0 with graphs and updated script

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 referred from https://github.com/bushvin/zabbix-kvm-res
 
+1. pip install libvirt-python or yum install python36-libvirt or apt install python3-libvirt  
+2. cp zabbix-kvm.py to /etc/zabbix/zabbix_agentd.d/scripts/zbx-kvm/
+3. cp userparameter_zabbix-kvm.conf to /etc/zabbix/zabbix_agentd2.d/
+4. import zabbix_kvm.yaml into zabbix frontend. I work in zabbix 7.0 (zabbix kvm.zml is old version without grpahs)
 
-1. pip install libvirt-python
-2. cp zabbix-kvm.py userparameter_zabbix-kvm.conf to /etc/zabbix/zabbix_agentd.d/
-3. import zabbix kvm.xml into zabbix frontend.I work in zabbix 3.X,perhaps it will be some troble in importing to zabbix2.X frontend
+Template provides vps load graphs: cpu / ram / disk / network.  
+The script no longer detects stopped domains. If a domain was stopped after getting into zabbix, then it will get null parameter values.  
+The script does not need to be run via sudo.  
+Data from the script is now output with the domain name instead of uuid. It seems to be more convenient to understand which virtual machine the graph belongs to.  
+Tested in VMManager 5 on centos 7 with python3.6  
--- zabbix-kvm.py
+++ zabbix-kvm.py
@@ -1,86 +1,119 @@
-#!/usr/local/bin/python3.5
+#!/usr/bin/env python3.6
 import libvirt
 import json
+import sys
 from optparse import OptionParser
-def main():
-  options = parse_args()
-  if options.item=="discovery":
-    uuid_list(options)
-  elif options.item=="cpu":
-    cpu(options)
-  elif options.item=="mem":
-    mem(options)
-  elif options.item=="net_out":
-    net_out(options)
-  elif options.item=="net_in":
-    net_in(options)
-  elif options.item=="rd_bytes":
-    rd_bytes(options)
-  elif options.item=="wr_bytes":
-    wr_bytes(options)
 
+def main():
+    options = parse_args()
+    if options.item == "discovery":
+        domain_list(options)
+    elif options.item == "cpu":
+        cpu(options)
+    elif options.item == "mem":
+        mem(options)
+    elif options.item == "net_out":
+        net_out(options)
+    elif options.item == "net_in":
+        net_in(options)
+    elif options.item == "rd_bytes":
+        rd_bytes(options)
+    elif options.item == "wr_bytes":
+        wr_bytes(options)
 
 def parse_args():
-  parser = OptionParser()
-  valid_item = ["discovery", "cpu","mem","net_out","net_in","wr_bytes","rd_bytes"]
-  parser.add_option("", "--item", dest="item", help="", action="store", type="string", default=None)
-  parser.add_option("", "--uuid", dest="uuid", help="", action="store", type="string", default=None)
-  (options, args) = parser.parse_args()
-  if options.item not in valid_item:
-    parser.error("Item has to be one of: "+", ".join(valid_item))
-  return options
+    parser = OptionParser()
+    valid_item = ["discovery", "cpu", "mem", "net_out", "net_in", "wr_bytes", "rd_bytes"]
+    parser.add_option("", "--item", dest="item", help="", action="store", type="string", default=None)
+    parser.add_option("", "--domain", dest="domain", help="", action="store", type="string", default=None)
+    (options, args) = parser.parse_args()
+    if options.item not in valid_item:
+        parser.error("Item has to be one of: " + ", ".join(valid_item))
+    return options
+
 def kvm_connect():
-  try:
-    conn = libvirt.openReadOnly('qemu:///system')
-  except:
-    sys.stderr.write("There was an error connecting to the local libvirt daemon using '"+uri+"'.")
-    exit(1)
-  return conn
-def uuid_list(options):
-  conn = kvm_connect()
-  r = {"data":[]}
-  try:
-    conn.listAllDomains(0)
-  except:
-    domains = []
-    for dom_id in conn.listDomainsID():
-      r['data'].append( {"{#DOMAINUUID}":conn.lookupByID(dom_id).UUIDString()} )
-  else:
+    try:
+        conn = libvirt.openReadOnly('qemu:///system')
+    except:
+        sys.stderr.write("There was an error connecting to the local libvirt daemon using 'qemu:///system'.")
+        sys.exit(1)
+    return conn
+
+def domain_list(options):
+    conn = kvm_connect()
+    r = {"data": []}
     for domain in conn.listAllDomains(0):
-      r["data"].append( {"{#DOMAINUUID}":domain.UUIDString()} )
-  print(json.dumps(r))
+        if domain.isActive():
+            r["data"].append({"{#DOMAINNAME}": domain.name()})
+    print(json.dumps(r))
+
 def cpu(options):
-  conn = kvm_connect()
-  cpunum=conn.lookupByUUIDString(options.uuid).info()[3]
-  cpuusertime=conn.lookupByUUIDString(options.uuid).info()[4]
-  r=cpuusertime/cpunum/10000000
-  print(r)  
+    conn = kvm_connect()
+    domain = conn.lookupByName(options.domain)
+    if not domain.isActive():
+        print(0)
+        return
+    cpunum = domain.info()[3]
+    cpuusertime = domain.info()[4]
+    r = cpuusertime / cpunum / 10000000
+    print(r)
+
 def mem(options):
-  conn = kvm_connect()
-  host=conn.lookupByUUIDString(options.uuid)
-  r=(host.memoryStats()["available"]-host.memoryStats()["unused"])/host.memoryStats()["available"]*100
-  print(r)
+    conn = kvm_connect()
+    domain = conn.lookupByName(options.domain)
+    if not domain.isActive():
+        print(0)
+        return
+    memstats = domain.memoryStats()
+    r = (memstats["available"] - memstats["unused"]) / memstats["available"] * 100
+    print(r)
+
+def extract_vnet_name(xml_desc):
+    import re
+    match = re.search(r"<target dev='(vnet\d+)'", xml_desc)
+    return match.group(1) if match else None
+
 def net_out(options):
-  conn = kvm_connect()
-  host=conn.lookupByUUIDString(options.uuid)
-  location=host.XMLDesc().find("<target dev=\'tap")
-  tapname=host.XMLDesc()[location+13:location+27]
-  print (host.interfaceStats("{0}".format(tapname))[4]*8)
+    conn = kvm_connect()
+    domain = conn.lookupByName(options.domain)
+    if not domain.isActive():
+        print(0)
+        return
+    tapname = extract_vnet_name(domain.XMLDesc())
+    if tapname:
+        print(domain.interfaceStats(tapname)[4] * 8)
+    else:
+        print(0)
+
 def net_in(options):
-  conn = kvm_connect()
-  host=conn.lookupByUUIDString(options.uuid)
-  location=host.XMLDesc().find("<target dev=\'tap")
-  tapname=host.XMLDesc()[location+13:location+27]
-  print (host.interfaceStats("{0}".format(tapname))[0]*8)
+    conn = kvm_connect()
+    domain = conn.lookupByName(options.domain)
+    if not domain.isActive():
+        print(0)
+        return
+    tapname = extract_vnet_name(domain.XMLDesc())
+    if tapname:
+        print(domain.interfaceStats(tapname)[0] * 8)
+    else:
+        print(0)
+
 def rd_bytes(options):
-  conn = kvm_connect()
-  host=conn.lookupByUUIDString(options.uuid)
-  r=host.blockStatsFlags("vda")['rd_bytes']
-  print(r)
+    conn = kvm_connect()
+    domain = conn.lookupByName(options.domain)
+    if not domain.isActive():
+        print(0)
+        return
+    r = domain.blockStatsFlags("vda")['rd_bytes']
+    print(r)
+
 def wr_bytes(options):
-  conn = kvm_connect()
-  host=conn.lookupByUUIDString(options.uuid)
-  r=host.blockStatsFlags("vda")['wr_bytes']
-  print(r)
+    conn = kvm_connect()
+    domain = conn.lookupByName(options.domain)
+    if not domain.isActive():
+        print(0)
+        return
+    r = domain.blockStatsFlags("vda")['wr_bytes']
+    print(r)
+
 if __name__ == "__main__":
-  main()
+    main()
